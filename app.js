227000fbd6710cc853405639eff8b4269f6149c4
Glauco Raupp Russo <glauco.developer@Glaucos-MacBook-Pro.local>
2019-02-23 13:40:18 +0000

    create getInput and getDOMStrings methods

--- app.js
+++ app.js
@@ -4,17 +4,43 @@ var budgetController = (function() {
 
 var UIController = (function(){
 
+    DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'        
+    }
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+        
+    };
 
 })();
 
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMStrings();
 
     var ctrlAddItem = function(){
 
+        // Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input)
+
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', function(event){
         
